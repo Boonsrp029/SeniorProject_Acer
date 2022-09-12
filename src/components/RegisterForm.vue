53f5fb73f93dd4bed2b5fc1d56fe3ee18d2d4761
Boonsrp029 <49550816+Boonsrp029@users.noreply.github.com>
2022-09-12 20:00:46 +0700

    Update RegisterForm.vue

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,25 +1,45 @@
 <template>
-  <div>
-    <h1>หน้าลงทะเบียนครุภัณฑ์</h1>
-  </div>
+  <h1>หน้าลงทะเบียนครุภัณฑ์</h1>
+  <form>
+    <div class="form-control">
+      <label for="equipment-name">ชื่อครุภัณฑ์/วัสดุคงทน</label>
+      <input type="text" v-model.trim="name">
+    </div>
+    <div class="form-control">
+      <label for="description">รายละเอียด</label>
+      <textarea v-model="description"></textarea>>
+    </div>
+    <div class="form-control">
+      <label for="quantity">จำนวน</label>
+      <input type="number" v-model="quantity">
+      <select>
+        <option value="เครื่อง">เครื่อง</option>
+        <option value="ตัว">ตัว</option>
+        <option value="ชุด">ชุด</option>
+        <option value="จอ">จอ</option>
+        <option value="ชิ้น">ชิ้น</option>
+        <option value="ใบ">ใบ</option>
+        <option value="อัน">อัน</option>
+        <option value="คัน">คัน</option>
+      </select>
+    </div>
+  </form>
 </template>
 
 <script>
 export default {
-  name:"RegisterForm",
-  data(){
+  name: "RegisterForm",
+  data() {
     return {
-      id:"",
-      type:"",
-      name:"",
-      description:"",
-      quantity:"",
-      price:"",
-      equipment_classification:"",
-      budget_classification:"",
-      budget_type:"",
-      purchase_method:"",
-      contract_number:""
+      name: "",
+      description: "",
+      quantity: "",
+      price: "",
+      equipment_type: "",
+      budget_classification: "",
+      budget_type: "",
+      purchase_method: "",
+      contract_number: ""
     }
   }
 }
