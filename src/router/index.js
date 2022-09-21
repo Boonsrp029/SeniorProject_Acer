4b1d86b05e6bca6023c8d4ed76d100aa883f1e14
Boonsrp029 <49550816+Boonsrp029@users.noreply.github.com>
2022-09-21 22:43:04 +0700

    Update Sep 21

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
+import LoginPage from '@/views/LoginPage.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/login',
+    name: 'login',
+    component: LoginPage
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // },
 ]
 
 const router = createRouter({
